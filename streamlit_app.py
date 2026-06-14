bc940460fa29c7fa41697587d6f1065a63f30218
BolishettySindhu <sindhubollishetty@gmail.com>
2026-06-14 22:01:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 st.title(":material/smart_toy: Hello, Cortex!")
 
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 prompt = st.text_input("Enter your prompt:")
 
